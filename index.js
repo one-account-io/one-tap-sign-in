4f713ec8a5783b9e4d8e7265f4eb62a62e215c87
Tim Tisak <tim.tisak@outlook.com>
2022-03-07 21:53:39 +0100

    1.0.4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 class OneAccountOneTapSignIn {
-  static _codeVerifier = '';
-  static isVisible = false;
+  constructor() {
+    this._codeVerifier = '';
+    this.isVisible = false;
+  }
 
-  static show = async ({
+  async show({
     clientId = '',
     redirectURI = '',
     apiURL = 'https://api.one-account.io/v1',
@@ -15,7 +17,7 @@ class OneAccountOneTapSignIn {
     codeChallenge,
     codeChallengeMethod = 'S256',
     saveCodeVerifier = false,
-  } = {}) => {
+  } = {}) {
     if (this.isVisible || document.getElementById('one-account-one-tap-sign-in')) return;
     this.isVisible = true;
 
@@ -45,28 +47,28 @@ class OneAccountOneTapSignIn {
       state,
       codeVerifier,
     };
-  };
+  }
 
-  static hide = () => {
+  hide() {
     this.isVisible = false;
     const element = document.getElementById('one-account-one-tap-sign-in');
     element.parentNode.removeChild(element);
-  };
+  }
 
-  static handleReceivedMessage = (event) => {
+  handleReceivedMessage(event) {
     if (event.data === 'removeOneAccountOneTapSignInIframe') {
       this.hide();
     }
-  };
+  }
 
-  static saveCodeVerifier = (codeVerifier) => {
+  saveCodeVerifier(codeVerifier) {
     this._codeVerifier = this.codeVerifier;
     const expires = new Date();
     expires.setTime(expires.getTime() + 3 * 60 * 60 * 1000);
     document.cookie = `codeVerifier=${codeVerifier}; expires=${expires.toUTCString()};path=/`;
-  };
+  }
 
-  static get codeVerifier() {
+  get codeVerifier() {
     if (!this._codeVerifier) {
       const name = 'codeVerifier' + '=';
       const ca = document.cookie.split(';');
@@ -85,20 +87,20 @@ class OneAccountOneTapSignIn {
     return this._codeVerifier;
   }
 
-  static generateCodeVerifier = (length = 128) => {
+  generateCodeVerifier(length = 128) {
     let text = '';
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
     for (let i = 0; i < length; i += 1) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-  };
+  }
 
-  static generateCodeChallenge = async (codeVerifier) => {
+  async generateCodeChallenge(codeVerifier) {
     const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(codeVerifier));
     const base64 = window.btoa(String.fromCharCode(...new Uint8Array(hash)));
     return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
-  };
+  }
 }
 
-export default OneAccountOneTapSignIn;
+export default new OneAccountOneTapSignIn();
